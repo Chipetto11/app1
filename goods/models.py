8caf1bcf3d40eefa950a0e21f2c71f68cfc986b2
App1_internet_store <chitashvilipeter@gmail.com>
2025-03-29 15:53:58 +0300

    created fixtures for goods

--- goods/models.py
+++ goods/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
+    name = models.CharField(max_length=150, unique=True, verbose_name='Название',)
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     
     
@@ -11,7 +11,8 @@ class Categories(models.Model):
         db_table = 'category'
         verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
-        
+    def __str__(self):
+        return self.name
     
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -19,7 +20,7 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name= "Описание")
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в процентах')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в процентах')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категории')
     
@@ -28,3 +29,6 @@ class Products(models.Model):
         db_table = 'product'
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+        
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
